7c8dc232e17d03167122e2a8da87676640aa077e
Leah Tynan <brunetto@alum.mit.edu>
2021-02-04 21:35:43 -0500

    Added bump animation for ox UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+	[Header("Oxen")]
+	public OxManager oxManager;
+
 	[Header("Song")]
-	// TODO: Songs will be stored elsewhere once the full architecture is planned
 	public string[] songNotes;
 
 	[Header("Musical Notes")]
@@ -27,14 +29,14 @@ public class GameManager : MonoBehaviour
 		{ "bf", 10 },
 		{ "b", 11 }
 	};
+	// TODO: Won't need the whole scale for Year of the Ox, just 4 notes (TBD) - will remove unneeded notes later
 
 	[Header("Colors")]
 	public ColorManager colorManager;
 	
 	void Start()
     {
-		//StartCoroutine(PlayScale(0.5f));
-		//StartCoroutine(CycleColors(1f));
+		StartCoroutine(PlayScale(0.5f));
 		//StartCoroutine(PlaySong(1f));
 	}
 
@@ -44,7 +46,6 @@ public class GameManager : MonoBehaviour
 		int octave = Convert.ToInt32(noteToPlay[0]);
 		int note = Convert.ToInt32(notesMapping[noteToPlay[1]]);
 		audioSource.clip = pianoKeysManager.notes[octave, note];
-		//colorManager.UpdateColor(octave, note);
 		audioSource.Play();
 	}
 
@@ -55,27 +56,28 @@ public class GameManager : MonoBehaviour
 		return mapping;
 	}
 
-	/*	The following are quick demos as proof of concept for iterating through music and color data structures */
-
-
 	IEnumerator PlaySong(float WaitTime)
 	{
 		for (int i = 0; i < songNotes.Length; i++)
         {
 			PlayNote(i);
+			//StartCoroutine(oxManager.oxen[row, column].Bump(WaitTime));
 			yield return new WaitForSeconds(WaitTime);
 		}
 	}
 
 	IEnumerator PlayScale(float WaitTime)
 	{
-		for (int i = 0; i < 3; i++)
+		int number = 0;
+		for (int i = 0; i < 2; i++)
 		{
-			for (int j = 0; j < 12; j++)
+			for (int j = 0; j < 4; j++)
 			{
 				audioSource.clip = pianoKeysManager.notes[i, j];
 				audioSource.Play();
-				yield return new WaitForSeconds(WaitTime);
+				StartCoroutine(oxManager.oxen[number].Bump(WaitTime));
+				yield return new WaitForSeconds(WaitTime * 2);
+				number++;
 			}
 		}
 	}
--- Assets/Scripts/OxViewer.cs
+++ Assets/Scripts/OxViewer.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 public class OxViewer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void ScaleUp()
-    {
-        // TODO: Bump up the size of the UI image
+    public IEnumerator Bump(float WaitTime)
+	{
+        Vector3 regularScale = this.transform.localScale;
+        Vector3 bumpedUpScale = new Vector3(1.25f, 1.25f, 1.0f);
+        float time = WaitTime/2;
+        float currentTime = 0.0f;
+        do
+        {
+            this.transform.localScale = Vector3.Lerp(regularScale, bumpedUpScale, currentTime / time);
+            currentTime += Time.deltaTime;
+            yield return null;
+        } while (currentTime <= time);
+		yield return new WaitForSeconds(WaitTime);
+		Debug.Log("Bumping down");
+        currentTime = 0.0f;
+        do
+        {
+            this.transform.localScale = Vector3.Lerp(bumpedUpScale, regularScale, currentTime / time);
+            currentTime += Time.deltaTime;
+            yield return null;
+        } while (currentTime <= time);
+        Debug.Log("Bump animation complete");
     }
 
     public void Sway()
